6aca1fd32db7509bebba371a8c7ff58f2a8adaa0
njartiana <48241768+njartiana@users.noreply.github.com>
2022-08-25 11:46:59 +0300

    Correction no new line at end of file

--- test_calculator.py
+++ test_calculator.py
@@ -5,12 +5,12 @@ class TestCalculator:
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-    
+
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-    
+
     def test_multiplication(self):
         assert 4 == calculator.multiplication(2, 2)
-    
+
     def test_division(self):
         assert 5 == calculator.division(25, 5)
