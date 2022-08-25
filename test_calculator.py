749f39d5d81bcd2c8092d77a92c8b1419dc969fb
njartiana <48241768+njartiana@users.noreply.github.com>
2022-08-25 11:34:10 +0300

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -8,10 +8,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-    
+
     def test_multiplication(self):
         assert 4 == calculator.multiplication(2, 2)
-    
+
     def test_division(self):
         assert 5 == calculator.division(25, 5)
-    
